8592d2dca9415c5748e37d5e9abb7c9997ce2291
Kevin <kevinmeagher1@hotmail.com>
2018-01-18 17:21:32 +1100

    inital commit, need to learn about SPAs in vue

---
+++ src/components/Login.vue
@@ -0,0 +1,69 @@
+<!-- <template>
+	<div id="login" class="tile ">
+		<h1 class="title">
+			Log In
+		</h1>
+		<p class="subtitle">
+			Enter Email and Password
+		</p>
+		<label class="label" for="email">Email:</label>
+		<input class="input" type="email" name="email" v-model="email" placeholder="email@sp.com">
+		<label class="label" for="password">Password:</label>
+		<input class="input" type="password" name="password" v-model="password" placeholder="skdjfglskjdgf">
+	</div>
+</template>
+ -->
+<template>
+	<section class="hero is-info is-fullheight">
+		<div class="hero-body">
+			<div class="container has-text-centered">
+				<div class="columns">
+					<div class="column is-half is-offset-one-quarter">
+						<h1 class="title">
+							VueToDo
+						</h1>
+						<h2 class="subtitle">
+							Log In
+						</h2>
+						<div class="column is-half is-offset-one-quarter">
+							<label class="label" for="email">Email:</label>
+							<input class="input" type="email" name="email" v-model="email" placeholder="email@sp.com">
+							<label class="label" for="password">Password:</label>
+							<input class="input" type="password" name="password" v-model="password" placeholder="skdjfglskjdgf">	
+						</div>
+						<button class="button is-primary" @click='submit'>Log In</button>
+					</div>
+				</div>		
+			</div>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	data(){
+		return{
+			email : "", 
+			password : ""
+		}
+	},
+	methods : {
+		submit () {
+			axios.post("http://back.vuetodo.com/api/login",{
+				"email" : this.email,
+				"password" : this.password
+			})
+			.then(function(data){
+				console.log("Sucess")
+			}).catch(function(data){
+				console.log("Failure");
+			})
+		}
+	}
+}
+</script>
+<style>
+.label{
+	color:white;
+}
+</style>
